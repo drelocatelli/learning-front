317c2526e8977ccc940253f06e3c54e92ecd5bb6
4ndr3224 <32282846+drelocatelli@users.noreply.github.com>
2022-01-28 20:54:28 -0300

    app

--- src/store/storeConfig.js
+++ src/store/storeConfig.js
@@ -1,4 +1,4 @@
-import {createStore, combineReducers} from 'redux';
+import {createStore, combineReducers, compose, applyMiddleware} from 'redux';
 import nomesReducers from './reducers/nomesReducers';
 import numerosReducers from './reducers/numerosReducers';
 
@@ -7,6 +7,34 @@ const reducers = combineReducers({
     nomes: nomesReducers
 });
 
-const storeConfig = () => createStore(reducers);
+// ----------------------- dev tools
+
+function logger({ getState }) {
+    return next => action => {
+      console.log('will dispatch', action)
+  
+      // Call the next dispatch method in the middleware chain.
+      const returnValue = next(action)
+  
+      console.log('state after dispatch', getState())
+  
+      // This will likely be the action itself, unless
+      // a middleware further in chain changed it.
+      return returnValue
+    }
+  }
+
+const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+const useDevTools = composeEnhancers(
+    applyMiddleware(logger),
+    // other store enhancers if any
+  );
+  
+// ----------------------- dev tools
+
+const storeConfig = () => createStore(
+    reducers, 
+    useDevTools
+);
 
 export default storeConfig;
